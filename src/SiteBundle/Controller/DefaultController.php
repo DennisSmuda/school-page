6d0df1094c23590e72dbdc74a2bc632f0a4fcfbe
Dennis Smuda <dennissmuda@msn.com>
2016-06-02 17:17:06 +0200

    Barba integration

--- src/SiteBundle/Controller/DefaultController.php
+++ src/SiteBundle/Controller/DefaultController.php
@@ -6,8 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
+    public function indexAction() {
         return $this->render('SiteBundle:Default:index.html.twig');
+    }
+
+    public function detailPageAction() {
+        return $this->render('@Site/Default/detailpage.html.twig');
     }
 }
